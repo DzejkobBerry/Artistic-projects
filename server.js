ca26e67e88bc855e65b5f1db052e96d450c7f4c0
DzejkobBerry <mcskunikpl@gmail.com>
2025-08-21 23:56:09 +0200

    Add vercel.json configuration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.DEVIL_NODEJS_PORT || 29384; // fallback na 29384
+const PORT = process.env.PORT || 3000; // zmień z DEVIL_NODEJS_PORT na PORT
 
 // Ustawienie silnika szablonów EJS
 app.set('view engine', 'ejs');
